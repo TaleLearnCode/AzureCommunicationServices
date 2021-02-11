6846a7079e24cef85b637620791c4c565601aeac
Chad Green <chadgreen@chadgreen.com>
2021-02-11 08:28:50 -0500

    Initial discovery work on how to send and receive SMS messages.

---
+++ TestFunction/TestFunction/SMSReceived.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestFunction
+{
+
+	public class SMSReceived
+	{
+		public string messageId { get; set; }
+		public string from { get; set; }
+		public string message { get; set; }
+		public string receivedTimestamp { get; set; }
+	}
+
+}
