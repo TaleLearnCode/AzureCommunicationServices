538b7e34d0ef06d09d61a1f4394927838876b4f9
Chad Green <chadgreen@chadgreen.com>
2021-02-15 10:37:37 -0500

    Updated the POC app for better usability

--- ProofOfConcept/Program.cs
+++ ProofOfConcept/Program.cs
@@ -1,24 +1,27 @@
 ﻿using System;
 using TaleLearnCode.CommunicationServices;
-using TaleLearnCode.CommunicationServices.Models;
 
 namespace ProofOfConcept
 {
-	class Program
+
+	public class Program
 	{
+
 		static void Main()
 		{
-			SendSMS();
+			do
+			{
+				SendSMS();
+			} while (true);
 		}
-
 
 
 		private static void SendSMS()
 		{
+
 			Console.Clear();
-			Console.WriteLine("Send SMS Demo");
-			Console.WriteLine("-------------");
-			Console.WriteLine();
+			PrintHeader();
+
 			Console.WriteLine("Phone Number (be sure to include +1):");
 			string phoneNumber = Console.ReadLine();
 			Console.WriteLine();
@@ -27,15 +30,31 @@ namespace ProofOfConcept
 
 			SMSService smsService = new SMSService(Settings.ACSConnectionString, Settings.AzureStorageSettings, Settings.MessageArchiveTable, Settings.ACSPhoneNumber);
 			string messageId = smsService.SendSMS(phoneNumber, message, true);
+
 			Console.WriteLine();
 			Console.WriteLine("Message sent");
 			Console.WriteLine($"Message ID: {messageId}");
 
+			Console.WriteLine();
 			Console.WriteLine("Press any key to continue...");
 			Console.ReadKey();
 
 		}
 
+		private static void PrintHeader()
+		{
+			ConsoleColor foregroundColor = Console.ForegroundColor;
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine(@"  _________                  .___   _________   _____    _________ ________                         ");
+			Console.WriteLine(@" /   _____/ ____   ____    __| _/  /   _____/  /     \  /   _____/ \______ \   ____   _____   ____  ");
+			Console.WriteLine(@" \_____  \_/ __ \ /    \  / __ |   \_____  \  /  \ /  \ \_____  \   |    |  \_/ __ \ /     \ /  _ \ ");
+			Console.WriteLine(@" /        \  ___/|   |  \/ /_/ |   /        \/    Y    \/        \  |    `   \  ___/|  Y Y  (  <_> )");
+			Console.WriteLine(@"/_______  /\___  >___|  /\____ |  /_______  /\____|__  /_______  / /_______  /\___  >__|_|  /\____/ ");
+			Console.WriteLine(@"        \/     \/     \/      \/          \/         \/        \/          \/     \/      \/        ");
+			Console.ForegroundColor = foregroundColor;
+			Console.WriteLine();
+			Console.WriteLine();
+		}
 
 
 	}
